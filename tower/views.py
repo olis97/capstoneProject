d3b19dfe682d0bb0a738181b802846aa386dc29d
olis97 <139129230+olis97@users.noreply.github.com>
2023-10-07 09:38:46 +0100

    added docs string for tower_about

--- tower/views.py
+++ tower/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+
 def tower_about(request):
+
+    """
+    Shows About page.
+
+    renders: tower_about.html
+
+    :param request: request
+    :type request: str
+
+    :return: Rendered HTML Webpage
+    :rtype: WebPage
+    """
+    
     return render(request, 'tower_about.html', {'current_page': 'about'})
 
 def tower_contact(request):
